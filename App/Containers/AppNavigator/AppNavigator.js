ce842c6245de970b7b371df5ab6f2c3e4a415281
syedibrahimt <ibrahimsyed624@gmail.com>
2020-05-28 01:04:36 +0530

    adding icons to the header

--- App/Containers/AppNavigator/AppNavigator.js
+++ App/Containers/AppNavigator/AppNavigator.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ShoppingScreen from '../ShoppingScreen/ShoppingScreen';
 import Style from './AppNavigatorStyle'
+import { Text, Image, View } from 'react-native';
+import Images from '../../Images/Images';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
@@ -10,7 +13,14 @@ const AppNavigator = () => {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="ShoppingScreen" component={ShoppingScreen} options={{
-                    title: 'Bag', headerStyle: Style.headerStyle
+                    title: 'Bag', headerTitleStyle: Style.titleStyle , headerStyle: Style.headerStyle, headerRight: () => (<View style={Style.headerRightStyle}>
+                        <TouchableOpacity onPress={()=> alert('link pressed')}>
+                            <Image source={Images.link} style={Style.linkIconStyle} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=> alert('notification pressed')}>
+                            <Image source={Images.notification} style={Style.linkIconStyle} />
+                        </TouchableOpacity>
+                    </View>)
                 }} />
             </Stack.Navigator>
         </NavigationContainer>
--- App/Containers/AppNavigator/AppNavigatorStyle.js
+++ App/Containers/AppNavigator/AppNavigatorStyle.js
@@ -1,7 +1,23 @@
 import { StyleSheet } from 'react-native'
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
 
 export default StyleSheet.create({
     headerStyle: {
         elevation: 0
+    },
+    linkIconStyle: {
+        height: wp(5),
+        width: wp(5),
+        tintColor: '#1269ff',
+        margin: wp(3),
+    },
+    headerRightStyle: {
+        flexDirection: 'row'
+    },
+    titleStyle:{
+        color: '#1269ff'
     }
 })
